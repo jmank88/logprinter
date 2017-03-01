124a469423fd45b56e9ae40d8f6d95fec71ee95c
Jordan Krage <jmank88@gmail.com>
2017-03-01 11:59:40 -0600

    code

---
+++ example_test.go
@@ -0,0 +1,34 @@
+package logprinter
+
+import (
+	"fmt"
+
+	"github.com/go-kit/kit/log"
+)
+
+func ExamplePrinter() {
+	var l log.Logger = &exampleLogger{}
+	p := NewPrinter(l, "msg")
+	p.Print("test")
+	p.Printf("%d", 10)
+	// Output:
+	// msg test
+	// msg 10
+}
+
+func ExampleContext() {
+	c := log.NewContext(&exampleLogger{}).With("key","val")
+	NewPrinter(c, "msg").Print("test")
+	c = c.WithPrefix("prefixKey", "prefixVal")
+	NewPrinter(c, "msg").Printf("%d", 10)
+	// Output:
+	// key val msg test
+	// prefixKey prefixVal key val msg 10
+}
+
+type exampleLogger struct {}
+
+func (l *exampleLogger) Log(keyvals ...interface{}) error {
+	_, err := fmt.Println(keyvals...)
+	return err
+}
---
+++ logprinter.go
@@ -0,0 +1,27 @@
+package logprinter
+
+import (
+	"github.com/go-kit/kit/log"
+	"fmt"
+)
+
+type Printer struct {
+	logger log.Logger
+	key    string
+}
+
+// NewPrinter returns a new Printer which logs messages as values of key.
+func NewPrinter(logger log.Logger, key string) *Printer {
+	return &Printer{
+		logger: logger,
+		key:    key,
+	}
+}
+
+func (p *Printer) Print(s string) error {
+	return p.logger.Log(p.key, s)
+}
+
+func (p *Printer) Printf(s string, a ...interface{}) error {
+	return p.logger.Log(p.key, fmt.Sprintf(s, a...))
+}
