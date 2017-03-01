0a7e313552ce80daebf4296e9bc5b802c13171ab
Jordan Krage <jmank88@gmail.com>
2017-03-01 12:07:18 -0600

    fix examples

--- example_test.go
+++ example_test.go
@@ -1,19 +1,31 @@
-package logprinter
+package logprinter_test
 
 import (
 	"fmt"
+
+	"github.com/jmank88/logprinter"
 
 	"github.com/go-kit/kit/log"
 )
 
 func ExamplePrinter() {
 	var l log.Logger = &exampleLogger{}
-	p := NewPrinter(l, "msg")
+	p := logprinter.NewPrinter(l, "msg")
 	p.Print("test")
 	p.Printf("%d", 10)
 	// Output:
 	// msg test
 	// msg 10
+}
+
+func ExamplePrinter_context() {
+	c := log.NewContext(&exampleLogger{}).With("key","val")
+	logprinter.NewPrinter(c, "msg").Print("test")
+	c = c.WithPrefix("prefixKey", "prefixVal")
+	logprinter.NewPrinter(c, "msg").Printf("%d", 10)
+	// Output:
+	// key val msg test
+	// prefixKey prefixVal key val msg 10
 }
 
 type exampleLogger struct {}
